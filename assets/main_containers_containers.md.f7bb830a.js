f6eda08f419419d44eb34d3fb7bd0bd32c136c3e
zakariachamakh <zakariachamakh32@gmail.com>
2021-10-13 00:13:49 +0100

    v1

---
+++ assets/main_containers_containers.md.f7bb830a.js
@@ -0,0 +1,20 @@
+import{_ as s,c as a,o as n,a as e}from"./app.46d0eaab.js";const m='{"title":"Examples","description":"","frontmatter":{},"headers":[{"level":2,"title":"Containers","slug":"containers"}],"relativePath":"main/containers/containers.md","lastUpdated":1633631234256}',i={},t=e(`<h1 id="examples" tabindex="-1">Examples <a class="header-anchor" href="#examples" aria-hidden="true">#</a></h1><h2 id="containers" tabindex="-1">Containers <a class="header-anchor" href="#containers" aria-hidden="true">#</a></h2><p>Custom containers can be defined by their types, titles, and contents.</p><div class="warning custom-block"><p class="custom-block-title">Input</p><pre><code>\u2B07\u2B07\u2B07\u2B07\u2B07\u2B07\u2B07\u2B07\u2B07\u2B07\u2B07\u2B07\u2B07\u2B07\u2B07\u2B07\u2B07\u2B07\u2B07\u2B07
+</code></pre></div><div class="language-md"><pre><code>
+::: tip
+This is a tip
+:::
+
+::: warning
+This is a warning
+:::
+
+::: danger
+This is a dangerous warning
+:::
+
+::: info
+This is an info box
+:::
+
+</code></pre></div><div class="tip custom-block"><p class="custom-block-title">Output</p><pre><code>\u2B07\uFE0F\u2B07\uFE0F\u2B07\uFE0F\u2B07\uFE0F\u2B07\uFE0F\u2B07\uFE0F\u2B07\uFE0F\u2B07\uFE0F\u2B07\uFE0F\u2B07\uFE0F\u2B07\uFE0F\u2B07\uFE0F
+</code></pre></div><hr><div class="tip custom-block"><p class="custom-block-title">TIP</p><p>This is a tip</p></div><div class="warning custom-block"><p class="custom-block-title">WARNING</p><p>This is a warning</p></div><div class="danger custom-block"><p class="custom-block-title">WARNING</p><p>This is a dangerous warning</p></div><div class="info custom-block"><p class="custom-block-title">INFO</p><p>This is an info box</p></div>`,11),c=[t];function o(r,p,d,l,h,u){return n(),a("div",null,c)}var b=s(i,[["render",o]]);export{m as __pageData,b as default};
---
+++ assets/main_index.md.a552fd82.lean.js
@@ -0,0 +1,26 @@
+import{_ as e,c as o,o as t,a as n}from"./app.46d0eaab.js";const h='{"title":"MarkDown Guide","description":"","frontmatter":{},"headers":[{"level":2,"title":"Guideline","slug":"guideline"}],"relativePath":"main/index.md","lastUpdated":1633631681610}',a={},c=n(`__VP_STATIC_START__<h1 id="markdown-guide" tabindex="-1">MarkDown Guide <a class="header-anchor" href="#markdown-guide" aria-hidden="true">#</a></h1><p>Guideline to create our MarkDown Documentation base on our Componentes :</p><h2 id="guideline" tabindex="-1">Guideline <a class="header-anchor" href="#guideline" aria-hidden="true">#</a></h2><blockquote><blockquote><div class="warning custom-block"><p class="custom-block-title">Input</p><p>\u2B07\u2B07\u2B07 <strong>The code To Create a components</strong> \u2B07\u2B07\u2B07</p></div></blockquote><div class="language-md"><pre><code>
+::: warning Input
+   \u2B07\u2B07\u2B07\u2B07\u2B07\u2B07\u2B07\u2B07\u2B07\u2B07\u2B07\u2B07\u2B07\u2B07\u2B07\u2B07\u2B07\u2B07\u2B07\u2B07
+:::
+
+\\\`\`\`md 
+
+<span class="token code-snippet code keyword">\`//Wirtte the Foo Same code Here\`</span>
+
+ Foo Same code Here
+
+\\\`\`\`
+
+
+::: tip Output
+   \u2B07\uFE0F\u2B07\uFE0F\u2B07\uFE0F\u2B07\uFE0F\u2B07\uFE0F\u2B07\uFE0F\u2B07\uFE0F\u2B07\uFE0F\u2B07\uFE0F\u2B07\uFE0F\u2B07\uFE0F\u2B07\uFE0F
+:::
+
+<span class="token code-snippet code keyword">\`//Wirtte the Foo Same code Here\`</span>
+
+Output Of the Foo  code 
+
+</code></pre></div><blockquote><div class="tip custom-block"><p class="custom-block-title">Output</p><p>\u2B07\uFE0F\u2B07\uFE0F\u2B07\uFE0F <strong>the Output how the componets well be see</strong> \u2B07\uFE0F\u2B07\uFE0F\u2B07\uFE0F</p></div></blockquote><div class="warning custom-block"><p class="custom-block-title">Input</p><p>\u2B07\u2B07\u2B07\u2B07\u2B07\u2B07\u2B07\u2B07\u2B07\u2B07\u2B07\u2B07\u2B07\u2B07\u2B07\u2B07\u2B07\u2B07\u2B07\u2B07</p></div><div class="language-md"><pre><code>
+Foo Same code Here
+
+</code></pre></div><div class="tip custom-block"><p class="custom-block-title">Output</p><p>\u2B07\uFE0F\u2B07\uFE0F\u2B07\uFE0F\u2B07\uFE0F\u2B07\uFE0F\u2B07\uFE0F\u2B07\uFE0F\u2B07\uFE0F\u2B07\uFE0F\u2B07\uFE0F\u2B07\uFE0F\u2B07\uFE0F</p></div><p>Output Of the Foo code</p></blockquote>__VP_STATIC_END__`,4),s=[c];function d(i,p,r,l,u,m){return t(),o("div",null,s)}var k=e(a,[["render",d]]);export{h as __pageData,k as default};
